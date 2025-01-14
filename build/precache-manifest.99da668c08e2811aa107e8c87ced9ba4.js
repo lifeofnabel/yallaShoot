029ffbb3e62a0b5b2ba6ff292a46952cd0751246
lifeofnabel <nabell.321@gmail.com>
2025-01-14 03:01:55 +0100

    Beta finished

---
+++ build/precache-manifest.99da668c08e2811aa107e8c87ced9ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ef21aed4e23feb9dca77368e33d689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9762171b507f52e5dc56",
+    "url": "/static/js/2.b0ae8250.chunk.js"
+  },
+  {
+    "revision": "940541ed879879447f4a12808880ec02",
+    "url": "/static/js/2.b0ae8250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f0ef1353f3ccf43879",
+    "url": "/static/js/main.410f3234.chunk.js"
+  },
+  {
+    "revision": "18329a68b6be07a6df68",
+    "url": "/static/js/runtime-main.607f5af0.js"
+  },
+  {
+    "revision": "3d99e581f8771789a72de6484c4015f5",
+    "url": "/static/media/logo.3d99e581.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b6d8fc9f7101a30fede918db358f00ac.js"
+  "/precache-manifest.99da668c08e2811aa107e8c87ced9ba4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
